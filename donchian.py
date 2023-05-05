0651e2fb73a38a6424f6be14ca15fc61e9460af7
Jay M Gonzalez <jaimungon@gmail.com>
2023-05-06 00:25:21 +0200

    finish donchian strategy

--- donchian.py
+++ donchian.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from binance.client import Client
 
 
 def donchian_breakout(df: pd.DataFrame, lookback: int):
@@ -11,6 +12,8 @@ def donchian_breakout(df: pd.DataFrame, lookback: int):
     df.loc[df["close"] < df["lower"], "signal"] = -1
     df["signal"] = df["signal"].ffill()
 
+    return df
+
 
 def signal_returns(df: pd.DataFrame, fee_amt: float = 0.001):
     df["log_return"] = np.log(df["close"]).diff().shift(-1)
@@ -19,6 +22,8 @@ def signal_returns(df: pd.DataFrame, fee_amt: float = 0.001):
     position_change = df["signal"].diff().abs()
     df["strategy_return"] -= position_change * fee_amt
     df["equity_curve"] = df["strategy_return"].cumsum()
+
+    return df
 
 
 def performance_metrics(df: pd.DataFrame, n_bars_in_year: int):
@@ -31,3 +36,58 @@ def performance_metrics(df: pd.DataFrame, n_bars_in_year: int):
     # annualized ratios
     sharpe *= n_bars_in_year**0.5
     sortino *= n_bars_in_year**0.5
+
+    return {
+        "Profit factor": profit_factor,
+        "Sharpe ratio": sharpe,
+        "Sortino ratio": sortino,
+    }
+
+
+def get_binance_data(symbol: str = "BTCUSDT") -> pd.DataFrame:
+    client = Client()
+
+    klines = client.get_historical_klines(
+        symbol, Client.KLINE_INTERVAL_1HOUR, "2 year ago UTC"
+    )
+
+    df = pd.DataFrame(
+        klines,
+        columns=[
+            "open_time",
+            "open",
+            "high",
+            "low",
+            "close",
+            "vol",
+            "close_time",
+            "quote_vol",
+            "trades",
+            "taker_base_vol",
+            "taker_quote_vol",
+            "ignore",
+        ],
+    )
+
+    df = df[["open_time", "open", "high", "low", "close"]]
+
+    df["open_time"] = pd.to_datetime(df["open_time"], unit="ms")
+
+    df["open"] = df.open.astype(float)
+    df["high"] = df.high.astype(float)
+    df["low"] = df.low.astype(float)
+    df["close"] = df.close.astype(float)
+
+    df.set_index("open_time", inplace=True)
+
+    df.to_csv("BTCUSDT.csv", index=True)
+
+    return df
+
+
+if __name__ == "__main__":
+    # df = get_binance_data()
+    df = pd.read_csv("BTCUSDT.csv")
+    df = donchian_breakout(df, 72)
+    df = signal_returns(df)
+    print(performance_metrics(df, 8760))
